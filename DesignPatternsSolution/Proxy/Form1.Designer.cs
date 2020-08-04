b5cf6770b5738da56af9af0a86fc034c74665afe
Edinei Correa de Menezes <edinei.menezes@gmail.com>
2020-08-04 19:11:46 -0300

    Sessão 3 completa ok

---
+++ DesignPatternsSolution/Proxy/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Proxy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProcessPayment = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.txtReceiptID = new System.Windows.Forms.TextBox();
+            this.txtAmountPaid = new System.Windows.Forms.TextBox();
+            this.txtDatePaid = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnProcessPayment
+            // 
+            this.btnProcessPayment.Location = new System.Drawing.Point(52, 31);
+            this.btnProcessPayment.Name = "btnProcessPayment";
+            this.btnProcessPayment.Size = new System.Drawing.Size(314, 28);
+            this.btnProcessPayment.TabIndex = 0;
+            this.btnProcessPayment.Text = "Process Payment";
+            this.btnProcessPayment.UseVisualStyleBackColor = true;
+            this.btnProcessPayment.Click += new System.EventHandler(this.btnProcessPayment_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(52, 163);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(314, 29);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // txtReceiptID
+            // 
+            this.txtReceiptID.Location = new System.Drawing.Point(154, 76);
+            this.txtReceiptID.Name = "txtReceiptID";
+            this.txtReceiptID.Size = new System.Drawing.Size(212, 20);
+            this.txtReceiptID.TabIndex = 2;
+            // 
+            // txtAmountPaid
+            // 
+            this.txtAmountPaid.Location = new System.Drawing.Point(154, 102);
+            this.txtAmountPaid.Name = "txtAmountPaid";
+            this.txtAmountPaid.Size = new System.Drawing.Size(212, 20);
+            this.txtAmountPaid.TabIndex = 3;
+            // 
+            // txtDatePaid
+            // 
+            this.txtDatePaid.Location = new System.Drawing.Point(154, 128);
+            this.txtDatePaid.Name = "txtDatePaid";
+            this.txtDatePaid.Size = new System.Drawing.Size(212, 20);
+            this.txtDatePaid.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Receipt ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Amount Paid";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Date Paid";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 254);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDatePaid);
+            this.Controls.Add(this.txtAmountPaid);
+            this.Controls.Add(this.txtReceiptID);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnProcessPayment);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProcessPayment;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.TextBox txtReceiptID;
+        private System.Windows.Forms.TextBox txtAmountPaid;
+        private System.Windows.Forms.TextBox txtDatePaid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ DesignPatternsSolution/flyweight/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace flyweight
+{
+    public class User
+    {
+        public Guid UserID { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<User> Followers { get; set; }
+
+        public static User GetUser(Guid userID)
+        {
+            ///get user from the database
+            ///demo user with random id
+            return new User
+            {
+                UserID = Guid.NewGuid()
+            };
+        }
+
+    }
+}
