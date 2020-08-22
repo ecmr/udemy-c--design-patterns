de431db464ae00fd1629bd026cdd84d99c3d9f22
Edinei Correa de Menezes <edinei.menezes@gmail.com>
2020-08-22 15:36:56 -0300

    curso concluido

--- DesignPatternsSolution/Visitor/Form1.Designer.cs
+++ DesignPatternsSolution/Visitor/Form1.Designer.cs
@@ -28,13 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.lblGreg = new System.Windows.Forms.Label();
+            this.lblRichard = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(32, 27);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(259, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add Credits";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(32, 70);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(259, 23);
+            this.btnUpdate.TabIndex = 1;
+            this.btnUpdate.Text = "Update Expiration";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // lblGreg
+            // 
+            this.lblGreg.AutoSize = true;
+            this.lblGreg.Location = new System.Drawing.Point(32, 119);
+            this.lblGreg.Name = "lblGreg";
+            this.lblGreg.Size = new System.Drawing.Size(63, 13);
+            this.lblGreg.TabIndex = 2;
+            this.lblGreg.Text = "Greg Status";
+            // 
+            // lblRichard
+            // 
+            this.lblRichard.AutoSize = true;
+            this.lblRichard.Location = new System.Drawing.Point(32, 150);
+            this.lblRichard.Name = "lblRichard";
+            this.lblRichard.Size = new System.Drawing.Size(77, 13);
+            this.lblRichard.TabIndex = 3;
+            this.lblRichard.Text = "Richard Status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(567, 285);
+            this.Controls.Add(this.lblRichard);
+            this.Controls.Add(this.lblGreg);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Label lblGreg;
+        private System.Windows.Forms.Label lblRichard;
     }
 }
 
--- DesignPatternsSolution/Visitor/Form1.cs
+++ DesignPatternsSolution/Visitor/Form1.cs
@@ -12,9 +12,31 @@ namespace Visitor
 {
     public partial class Form1 : Form
     {
+        private Users Users = new Users();
+        private User Greg = new User("Greg", DateTime.Now, 25);
+        private User Richard = new User("Richard", DateTime.Now.AddMonths(-6), 42);
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Users.Attach(Greg);
+            Users.Attach(Richard);
+            UpdateStatus();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Users.Accept(new ExpirationVisitor());
+            UpdateStatus();
+        }
+
+        private void UpdateStatus()
+        {
+            lblGreg.Text = Greg.Name + ": " + Greg.Credits.ToString() + " credits expire " + Greg.ExpirationDate.ToString();
+            lblRichard.Text = Richard.Name + ": " + Richard.Credits.ToString() + " credits expire " + Richard.ExpirationDate.ToString();
         }
     }
 }
